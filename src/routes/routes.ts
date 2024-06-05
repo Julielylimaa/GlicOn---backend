26fdfd657038044f1293169496fbef17706a7f23
Juliely <juhlima26@icloud.com>
2024-06-05 11:06:47 -0300

    feat: :sparkles: adds JWT

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,11 +2,11 @@ import { register } from "../http/controllers/register"
 import { AppDataSource } from "../data-source"
 import { User } from "../entity/User"
 import { express } from "../server"
-import { login } from "../http/controllers/login"
+import { getProfile, login } from "../http/controllers/login"
 const routes = express.Router()
 
 
 routes.post('/login', login)
 routes.post('/register', register)
-
+routes.get('/profile', getProfile)
 module.exports = routes
